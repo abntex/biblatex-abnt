b4c30494673d7f45d20422f2c3698a629ececbe3
Daniel M <danielbmarques@gmail.com>
2017-11-30 02:10:15 -0200

    Try to make tests faster

--- tests/test.sh
+++ tests/test.sh
@@ -4,27 +4,16 @@ set -e
 
 echo "Running tests..."
 
-sed -i.bak 's/\\toggletrue{reference}/% \\toggletrue{reference}/' NBR10520-2002.tex
-
-sed -i.bak 's/\\toggletrue{reference}/% \\toggletrue{reference}/' NBR6023-2002.tex
-
-latexmk -pdf -norc -interaction=nonstopmode NBR10520-2002.tex
-
-latexmk -pdf -norc -interaction=nonstopmode NBR6023-2002.tex
-
-pdflatex -interaction=nonstopmode NBR10520-2002_test.tex
-
-pdflatex -interaction=nonstopmode NBR6023-2002_test.tex
-
-sed -i.bak 's/% \\toggletrue{reference}/\\toggletrue{reference}/' NBR10520-2002.tex
-
-sed -i.bak 's/% \\toggletrue{reference}/\\toggletrue{reference}/' NBR6023-2002.tex
-
-rm NBR10520-2002.tex.bak
-
-rm NBR6023-2002.tex.bak
-
 pass=true
+
+
+sed -i.bak 's/\\toggletrue{reference}/% \\toggletrue{reference}/' NBR10520-2002.tex
+pdflatex -interaction=nonstopmode NBR10520-2002.tex
+biber NBR10520-2002
+pdflatex -interaction=nonstopmode NBR10520-2002.tex
+pdflatex -interaction=nonstopmode NBR10520-2002_test.tex
+pdflatex -interaction=nonstopmode NBR10520-2002_test.tex
+sed -i.bak 's/% \\toggletrue{reference}/\\toggletrue{reference}/' NBR10520-2002.tex
 
 while read -r line ; do
     C=$(echo $line | awk '{print $1}')
@@ -32,15 +21,26 @@ while read -r line ; do
     M=$(echo $line | awk '{print $3}')
     if [ "$C" != 0.00000 ] || [ "$Y" != 0.00000 ] || [ "$M" != 0.00000 ] ; then
         pass=false
+        echo "There's something wrong in NBR10520-2002_test.pdf"
     fi
 done < <(gs -q  -o - -sDEVICE=inkcov NBR10520-2002_test.pdf | sed -e '1d')
 
+
+sed -i.bak 's/\\toggletrue{reference}/% \\toggletrue{reference}/' NBR6023-2002.tex
+pdflatex -interaction=nonstopmode NBR6023-2002.tex
+biber NBR6023-2002
+pdflatex -interaction=nonstopmode NBR6023-2002.tex
+pdflatex -interaction=nonstopmode NBR6023-2002_test.tex
+pdflatex -interaction=nonstopmode NBR6023-2002_test.tex
+sed -i.bak 's/% \\toggletrue{reference}/\\toggletrue{reference}/' NBR6023-2002.tex
+
 while read -r line ; do
     C=$(echo $line | awk '{print $1}')
     Y=$(echo $line | awk '{print $2}')
     M=$(echo $line | awk '{print $3}')
     if [ "$C" != 0.00000 ] || [ "$Y" != 0.00000 ] || [ "$M" != 0.00000 ] ; then
         pass=false
+        echo "There's something wrong in NBR6023-2002_test.pdf"
     fi
 done < <(gs -q  -o - -sDEVICE=inkcov NBR6023-2002_test.pdf | sed -e '1d')
 
@@ -49,7 +49,6 @@ if [ "$pass" = true ] ; then
     echo "ALL GOOD!"
     exit 0
 else
-    echo "OOPS, THERE'S SOMETHING WRONG!"
     exit 1
 fi
 
---
+++ tests/texlive.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+tar -xzf install-tl-unx.tar.gz
+cd install-tl-20*
+
+cat << EOF >> texlive.profile
+selected_scheme scheme-basic
+TEXDIR /tmp/texlive
+TEXMFCONFIG ~/.texlive/texmf-config
+TEXMFHOME ~/texmf
+TEXMFLOCAL /tmp/texlive/texmf-local
+TEXMFSYSCONFIG /tmp/texlive/texmf-config
+TEXMFSYSVAR /tmp/texlive/texmf-var
+TEXMFVAR ~/.texlive/texmf-var
+option_doc 0
+option_src 0
+EOF
+
+./install-tl --profile=./texlive.profile
+export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
+
+sudo env "PATH=$PATH" tlmgr update --self --all
+
+sudo env "PATH=$PATH" tlmgr install collection-latexrecommended collection-fontsrecommended collection-langportuguese biblatex biber logreq xstring xpatch csquotes substr pdfpagediff
+
+mkdir /tmp/texlive/texmf-local/tex/latex/biblatex-contrib
+sudo ln -s $TRAVIS_BUILD_DIR/abntex/biblatex-abnt /tmp/texlive/texmf-local/tex/latex/biblatex-contrib/biblatex-abnt
+sudo env "PATH=$PATH" texhash
+
+cd $TRAVIS_BUILD_DIR/abntex/biblatex-abnt
+
