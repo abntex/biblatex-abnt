2cb56c5601f4ee5c863915d27e860138b7c52ee7
Daniel M <danielbmarques@gmail.com>
2017-11-30 14:48:35 -0200

    Use scheme-minimal for tests

--- tests/texlive.sh
+++ tests/texlive.sh
@@ -5,7 +5,7 @@ tar -xzf install-tl-unx.tar.gz
 cd install-tl-20*
 
 cat << EOF >> texlive.profile
-selected_scheme scheme-basic
+selected_scheme scheme-minimal
 TEXDIR /tmp/texlive
 TEXMFCONFIG ~/.texlive/texmf-config
 TEXMFHOME ~/texmf
@@ -20,7 +20,8 @@ EOF
 ./install-tl --profile=./texlive.profile
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
 
-tlmgr update --self --all
-
-tlmgr install collection-latexrecommended collection-fontsrecommended collection-langportuguese biblatex biber logreq xstring xpatch csquotes substr pdfpagediff
+tlmgr install latex-bin lm ec biblatex biber babel babel-portuges \
+    hyphen-portuguese logreq xstring xpatch csquotes substr pdfpagediff \
+    microtype graphics etoolbox xcolor oberdiek url l3packages hyperref \
+    geometry l3kernel
 
