abdb2994d5cecc9a2a0929435cb211e4af0ed636
Daniel M <danielbmarques@gmail.com>
2017-11-30 02:18:04 -0200

    Fix travis build path

--- tests/texlive.sh
+++ tests/texlive.sh
@@ -25,8 +25,8 @@ sudo env "PATH=$PATH" tlmgr update --self --all
 sudo env "PATH=$PATH" tlmgr install collection-latexrecommended collection-fontsrecommended collection-langportuguese biblatex biber logreq xstring xpatch csquotes substr pdfpagediff
 
 mkdir /tmp/texlive/texmf-local/tex/latex/biblatex-contrib
-sudo ln -s $TRAVIS_BUILD_DIR/abntex/biblatex-abnt /tmp/texlive/texmf-local/tex/latex/biblatex-contrib/biblatex-abnt
+sudo ln -s $TRAVIS_BUILD_DIR /tmp/texlive/texmf-local/tex/latex/biblatex-contrib/biblatex-abnt
 sudo env "PATH=$PATH" texhash
 
-cd $TRAVIS_BUILD_DIR/abntex/biblatex-abnt
+cd $TRAVIS_BUILD_DIR
 
