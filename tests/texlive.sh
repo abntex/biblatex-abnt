e6ca7419b5484344f5b667e976e4e06019d6f5ea
Daniel M <danielbmarques@gmail.com>
2017-11-30 02:24:48 -0200

    Move some stuff from texlive.sh to .travis.yml

--- tests/texlive.sh
+++ tests/texlive.sh
@@ -24,9 +24,3 @@ sudo env "PATH=$PATH" tlmgr update --self --all
 
 sudo env "PATH=$PATH" tlmgr install collection-latexrecommended collection-fontsrecommended collection-langportuguese biblatex biber logreq xstring xpatch csquotes substr pdfpagediff
 
-mkdir /tmp/texlive/texmf-local/tex/latex/biblatex-contrib
-sudo ln -s $TRAVIS_BUILD_DIR /tmp/texlive/texmf-local/tex/latex/biblatex-contrib/biblatex-abnt
-sudo env "PATH=$PATH" texhash
-
-cd $TRAVIS_BUILD_DIR
-
