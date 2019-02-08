062b645d3d9bade8f70550d8141b743caa726283
Daniel M <danielbmarques@gmail.com>
2019-02-08 17:00:26 -0200

    Revert "Add pdfpages to texlive installation" [ci skip]
    This reverts commit e283de7e7c6244db12a6339cda4ed974e108a447.

--- tests/texlive.sh
+++ tests/texlive.sh
@@ -21,7 +21,7 @@ EOF
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
 
 tlmgr --no-verify-downloads install latex-bin lm ec biblatex biber babel babel-portuges \
-    hyphen-portuguese logreq xstring xpatch csquotes substr pdfpagediff pdfpages \
+    hyphen-portuguese logreq xstring xpatch csquotes substr pdfpagediff \
     microtype graphics etoolbox xcolor oberdiek url l3packages hyperref \
     geometry l3kernel
 
